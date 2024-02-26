48c270969969ecde992abf051deef64dd75c79a6
MakPro <maks.prokopenko@oneplusone.solutions>
2024-02-26 12:09:26 +0200

    first steps

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { useEffect } from 'react';
+
 import './App.css';
 
+const tg = window.Telegram.WebApp;
+
 function App() {
+  useEffect(() => {
+    tg.ready();
+  }, []);
+
   return (
     <>
       <h2>Telegram Bot App</h2>
+      <button onClick={() => tg.close()}>Close</button>
     </>
   );
 }
