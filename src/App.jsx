0cc98c636daf177a4de566ec70e658a71b7faee5
MakPro <maks.prokopenko@oneplusone.solutions>
2024-02-29 09:06:16 +0200

    main button

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,14 @@ import './App.css';
 const tg = window.Telegram.WebApp;
 
 function App() {
+  const toggleMainButton = () => {
+    if (tg.MainButton.isVisible) {
+      tg.MainButton.hide();
+    } else {
+      tg.MainButton.show();
+    }
+  };
+
   useEffect(() => {
     tg.ready();
     tg.expand();
@@ -14,6 +22,7 @@ function App() {
     <>
       <h2>Telegram Bot App</h2>
       <button onClick={() => tg.close()}>Close</button>
+      <button>Main Button</button>
     </>
   );
 }
